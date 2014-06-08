36bc0af30c9a38e1a9bca5aa4a4e4984850cf93d
christopherfricke <christopher.fricke@bmoregeo.com>
2014-06-08 08:53:55 -0400

    update mapnik version

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name        "tilestache"
 description 'Cookbook to install TileStache vector tile server'
 maintainer  "cfricke"
 license     "Apache 2.0"
-version     "1.0.5"
+version     "1.0.6"
 
 depends 'apt'
 depends 'python'
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,11 @@
+apt_repository 'mapnik' do
+	uri "http://ppa.launchpad.net/mapnik/v2.2.0/ubuntu"
+	distribution node['lsb']['codename']
+	components ['main']
+	keyserver 'keyserver.ubuntu.com'
+	key "5D50B6BA"
+end
+
 package "python-dev"
 
 for pil_dependency in ["libjpeg-dev", "libjpeg-dev", "libfreetype6", "libfreetype6-dev", "zlib1g-dev"] do
@@ -10,13 +18,7 @@ python_pip "modestmaps"
 python_pip "simplejson"
 python_pip "werkzeug"
 
-apt_repository 'mapnik' do
-	uri "http://ppa.launchpad.net/mapnik/v2.1.0/ubuntu"
-	distribution node['lsb']['codename']
-	components ['main']
-	keyserver 'keyserver.ubuntu.com'
-	key "5D50B6BA"
-end
+
 
 for pkg in ['libmapnik', 'libmapnik-dev', 'mapnik-utils', 'python-mapnik'] do
 	package pkg
