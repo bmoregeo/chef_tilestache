d2bee84537d330c8460fa42bebd63c67077bc150
christopherfricke <christopher.fricke@bmoregeo.com>
2014-06-08 09:08:14 -0400

    remove adding the mapnik repository

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name        "tilestache"
 description 'Cookbook to install TileStache vector tile server'
 maintainer  "cfricke"
 license     "Apache 2.0"
-version     "1.0.6"
+version     "1.0.7"
 
 depends 'apt'
 depends 'python'
