de132e40c498377fdcb301e538a9ed7af0858bcf
christopherfricke <christopher.fricke@bmoregeo.com>
2014-06-10 21:10:15 -0400

    update version

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name        "tilestache"
 description 'Cookbook to install TileStache vector tile server'
 maintainer  "cfricke"
 license     "Apache 2.0"
-version     "1.0.8"
+version     "1.0.9"
 
 depends 'apt'
 depends 'python'
